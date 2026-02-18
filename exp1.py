cbf73903801ff0417c9fb282a07db60758f30bc3
Humairah_shifa <amanshifa242007@gmail.com>
2026-02-18 14:16:10 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+data = {
+'Name': ['Alice', 'Bob', 'Charlie'],
+'Age': [25, 30, 35],
+'City': ['New York', 'Los Angeles', 'Chicago']
+}
+df = pd.DataFrame(data)
+print(df.head())
+filtered_df = df[df['Age'] > 30]
+print(filtered_df)
+df['Senior'] = df['Age'] > 30
+print(df)
+grouped_df = df.groupby('City')['Age'].mean()
+print(grouped_df)
+
+
+# In[ ]:
+
+
+
+
